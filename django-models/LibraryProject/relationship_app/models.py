f067783d129b037fef019bb3fff29dca7289c1d7
Zola <126443383+zolakulani@users.noreply.github.com>
2025-11-15 14:07:51 +0200

    Fix Librarian field: rename 'books' to 'library'

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -24,7 +24,7 @@ class Library(models.Model):
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-    books = models.OneToOneField(Library, on_delete=models.CASCADE)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
